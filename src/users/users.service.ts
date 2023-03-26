e9ac28f5b6406b64f28a2761fe9309648ada62b2
Mauricio <mauricionatacci98@gmail.com>
2023-03-26 13:01:15 -0300

    add: entity, module, service and controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
+  }
+
+  async create(user: User): Promise<User> {
+    const newUser = this.usersRepository.create(user);
+    return await this.usersRepository.save(newUser);
+  }
+
+  async update(id: number, user: User): Promise<User> {
+    await this.usersRepository.update({ id }, user);
+    return await this.usersRepository.findOne({ where: { id } });
+  }
+
+  async delete(id: number): Promise<void> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    await this.usersRepository.delete({ id });
+  }
+}
